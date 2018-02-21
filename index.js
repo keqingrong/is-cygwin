9d41b410129ebbdcb0281378d8919c47906ff7b8
Qingrong Ke <keqingrong1992@gmail.com>
2018-02-21 17:38:06 +0800

    It is enough to get kernel name

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const isCygwin = () => {
   }
 
   try {
-    return execSync('uname -a', {encoding: 'utf-8'}).toLowerCase().includes('cygwin');
+    // Reference:
+    // http://man7.org/linux/man-pages/man1/uname.1.html
+    // https://en.wikipedia.org/wiki/Uname
+    return execSync('uname', {encoding: 'utf-8'}).toLowerCase().includes('cygwin');
   } catch (err) {
     return false;
   }
