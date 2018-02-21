c1758703058b50e19557561904824836eea6bec6
Qingrong Ke <keqingrong1992@gmail.com>
2018-02-21 17:19:42 +0800

    Use uname instead of /proc/version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const fs = require('fs');
+const { execSync } = require('child_process');
 
 const isCygwin = () => {
   if (process.platform !== 'win32') {
@@ -7,7 +7,7 @@ const isCygwin = () => {
   }
 
   try {
-    return fs.readFileSync('/proc/version', 'utf8').toLowerCase.includes('cygwin');
+    return execSync('uname -a', {encoding: 'utf-8'}).toLowerCase().includes('cygwin');
   } catch (err) {
     return false;
   }
